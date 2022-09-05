7061be05bd428809a706204c2ec2ac27aa96f410
hayanalmulla <hayanasalmulla@hotmail.com>
2022-09-05 19:39:24 +0300

    empty

---
+++ cw1/cw1/cw1App.swift
@@ -0,0 +1,17 @@
+//
+//  cw1App.swift
+//  cw1
+//
+//  Created by macbook on 05/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct cw1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
