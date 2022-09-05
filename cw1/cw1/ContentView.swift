42ec9d9ac3a042639fdc4c12756d29dd0630c8e7
hayanalmulla <hayanasalmulla@hotmail.com>
2022-09-05 20:41:42 +0300

    submit

--- cw1/cw1/ContentView.swift
+++ cw1/cw1/ContentView.swift
@@ -9,8 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Image("c1")
+                .resizable()
+                .ignoresSafeArea()
+            VStack {
+                Text("Hello, My Name is HAYA").padding()
+                    .font(.title2)
+                    .foregroundColor(.white)
+                Text("I Am 23 years old").padding()
+                    .font(.title2)
+                    .foregroundColor(.white)
+                Text("I am Learning SwiftUI now").padding()
+                    .font(.title2)
+                    .foregroundColor(.white)
+                HStack{
+                    Image(systemName: "heart.fill")
+                        .foregroundColor(.white)
+                        
+                        .frame(width: 20, height: 20)
+                    Spacer()
+                    Image(systemName: "gear")
+                        .foregroundColor(.white)
+//                        .resizable()
+                        .frame(width: 20, height: 20)
+                    Spacer()
+                    Image(systemName: "trash.fill")
+                        .foregroundColor(.white)
+                        
+                        .frame(width: 20, height: 20)
+                    
+                }
+            }
+            
+        }
+        
     }
 }
 
